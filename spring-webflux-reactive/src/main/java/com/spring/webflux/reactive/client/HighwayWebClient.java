e548ca2c37d3fa2609dadcf1b08cbec0a6d37f4d
ualter.junior@gmail.com <ualter.junior@gmail.com>
2018-04-12 23:02:20 +0200

    .

--- spring-webflux-reactive/src/main/java/com/spring/webflux/reactive/client/HighwayWebClient.java
+++ spring-webflux-reactive/src/main/java/com/spring/webflux/reactive/client/HighwayWebClient.java
@@ -8,57 +8,55 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import com.spring.webflux.reactive.model.Vehicle;
 
+import reactor.core.scheduler.Schedulers;
+
 public class HighwayWebClient {
 
-	private WebClient webClient = WebClient.builder().baseUrl("http://localhost:8080").build();
+    private WebClient webClient = WebClient.builder()
+        .baseUrl("http://localhost:8080")
+        .build();
 
-	public void vehicleDetected() {
-		AtomicInteger counter = new AtomicInteger(0);
-		//@formatter:off
-	        webClient.get()
-	            .uri("/vehicles")
-	            .accept(MediaType.APPLICATION_STREAM_JSON)
-	            .exchange()
-	            //.publishOn(Schedulers.single())
-	            .flatMapMany(response -> response.bodyToFlux(Vehicle.class))
-	            //.delayElements(Duration.ofMillis(1))
-	            .subscribe(s -> {
-	                    System.out.println(counter.incrementAndGet() + " >>>>>>>>>>>>> " + s);
-	                },
-	                err -> System.out.println("Error on Weather Stream: " + err),
-	                () -> System.out.println("Vehicle stream stoped!"));
-	      //@formatter:on
+    public void vehicleDetected() {
+        AtomicInteger counter = new AtomicInteger(0);
+        webClient.get()
+            .uri("/vehicles")
+            .accept(MediaType.APPLICATION_STREAM_JSON)
+            .exchange()
+            .publishOn(Schedulers.single())
+            .flatMapMany(response -> response.bodyToFlux(Vehicle.class))
+            .delayElements(Duration.ofMillis(1000))
+            .subscribe(s -> {
+                    System.out.println(counter.incrementAndGet() + " >>>>>>>>>> " + s);
+                },
+                err -> System.out.println("Error on Weather Stream: " + err),
+                () -> System.out.println("Vehicle stream stoped!"));
+    }
 
-	}
-	
-	public void vehicleHigherThen120Detected() {
-		AtomicInteger counter = new AtomicInteger(0);
-		//@formatter:off
-	        webClient.get()
-	            .uri("/vehicles")
-	            .accept(MediaType.APPLICATION_STREAM_JSON)
-	            .exchange()
-	            .flatMapMany(response -> response.bodyToFlux(Vehicle.class))
-	            .filter(v -> v.getSpeed() > 120)
-	            //.delayElements(Duration.ofMillis(1))
-	            .subscribe(s -> {
-	                    System.out.println(counter.incrementAndGet() + " >>>>>>>>>>>>> " + s);
-	                },
-	                err -> System.out.println("Error on Weather Stream: " + err),
-	                () -> System.out.println("Vehicle stream stoped!"));
-	      //@formatter:on
+    public void vehicleHigherThen120Detected() {
+        AtomicInteger counter = new AtomicInteger(0);
+        webClient.get()
+        .uri("/vehicles")
+        .accept(MediaType.APPLICATION_STREAM_JSON)
+        .exchange()
+        .flatMapMany(response -> response.bodyToFlux(Vehicle.class))
+        .filter(v -> v.getSpeed() > 120)
+        .delayElements(Duration.ofMillis(250))
+        .subscribe(s -> {
+                System.out.println(counter.incrementAndGet() + " >>>>>>>>>> " + s);
+            },
+            err -> System.out.println("Error on Weather Stream: " + err),
+            () -> System.out.println("Vehicle stream stoped!"));
+    }
 
-	}
-
-	public static void main(String[] args) {
-		HighwayWebClient vehiclesWebClient = new HighwayWebClient();
-		//vehiclesWebClient.vehicleHigherThen120Detected();
-		vehiclesWebClient.vehicleDetected();
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        HighwayWebClient vehiclesWebClient = new HighwayWebClient();
+        vehiclesWebClient.vehicleHigherThen120Detected();
+        //vehiclesWebClient.vehicleDetected();
+        try {
+            Thread.sleep(35000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
